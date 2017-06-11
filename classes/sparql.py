1044f0d547c239819db3a10bea7a5741699b7bf3
dushiel <mannetje_miedema@hotmail.com>
2017-06-11 18:57:05 +0200

    Create sparql.py

--- classes/sparql.py
+++ classes/sparql.py
@@ -25,8 +25,8 @@ class ValueQuery(SparqlQuery):
 
     def __init__(self, entity_ID, property_ID):
         self.query = '''
-        SELECT ?property ?propertyLabel WHERE {
-           wd:'''+entity_ID+''' wdt:'''+property_ID+'''  ?property.
+        SELECT ?answer ?answerLabel WHERE {
+           wd:'''+entity_ID+''' wdt:'''+property_ID+'''  ?answer.
            SERVICE wikibase:label {
              bd:serviceParam wikibase:language "en" .
            }
