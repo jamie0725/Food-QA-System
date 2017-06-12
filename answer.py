f8f66062938282dc8826e893056749f311631e5e
daniel <mannetje_miedema@hotmail.com>
2017-06-12 22:24:03 +0200

    fixed count question (!=0)

--- answer.py
+++ answer.py
@@ -95,9 +95,10 @@ class Answer:
                 continue
             query = queryConstructor(entity_id, property_id)
             answer = query.get()
-            if answer:
-                self.answers = answer
-                return
+            if answer[0]!="0":
+                if answer:
+                    self.answers = answer
+                    return
                 
     def get_answer_boolean(self, entities_and_properties_and_entities, queryConstructor):
         for entity_id, property_id, entity_id2 in entities_and_properties_and_entities:
