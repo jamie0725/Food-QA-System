bdf968282cdc289337b89296f0731af36e6aaf7f
Bart Louwers <bart.louwers@gmail.com>
2017-06-12 19:29:08 +0200

    anchor_texts

--- qa_system.py
+++ qa_system.py
@@ -5,6 +5,7 @@ from count import Count
 from answer import Answer
 import logging
 import argparse
+import wikipedia
 
 parser = argparse.ArgumentParser(description='This is a demo.')
 parser.add_argument("-l", "--log", dest="logLevel", choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], help="Set the logging level")
@@ -16,10 +17,11 @@ if args.logLevel:
 try:
     # show all logging with level >= DEBUG
     nlp = spacy.load('en')
+    anchor_texts = wikipedia.AnchorTexts('anchor_texts')
     print_count = Count()
     for line in sys.stdin:
         question = Question(line, nlp)
-        answer = Answer(question, print_count, nlp)
+        answer = Answer(question, print_count, nlp, anchor_texts)
         answer.print_it() 
 except KeyboardInterrupt: # ctrl+c won't return an error
     pass
--- wikipedia.py
+++ wikipedia.py
@@ -1,6 +1,21 @@
-
+import subprocess
+import pathlib
+import re
+import logging
 
 class AnchorTexts:
-    def __init__(self, filename):
+    def __init__(self, filepath):
+        self.path = pathlib.Path(filepath)
+        
+        if not self.path.is_file():
+            raise FileNotFoundError
 
-    
+    def get_URLs(self, term):
+        pipe = subprocess.Popen(['grep', '^' + term, 'anchor_texts'], stdout=subprocess.PIPE)
+        wikipages = []
+        for line in pipe.stdout:
+            wikipages.append(re.split(r'\t+', line.strip().decode("utf-8")))
+        wikipages.sort(key=lambda arr: int(arr[2]), reverse=True)
+        logging.debug('Found wikipages for {}:\n'.format(term) +
+                '\n'.join('- {}'.format(*p) for p in wikipages[:10]))
+        return wikipages[:10]
